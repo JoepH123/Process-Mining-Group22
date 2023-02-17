7a5af7164b7e63bd078aa4beb6a9b0703c40b1ce
Asen Dotsinski <adotsinski@gmail.com>
2023-02-17 17:06:51 +0200

    refactor: add comments, connect submodules

--- baseline.py
+++ baseline.py
@@ -2,55 +2,7 @@
 import copy
 import pandas as pd
 from sklearn.metrics import accuracy_score, mean_absolute_error
-
-def load_dataset():
-    """Loads the dataset, fixes the timestamps and adds columns 
-    for actual results.
-
-    The expected input is the output of the "xes2csv.jar" file.
-
-    :return: A tuple containing the training, validation and test data.
-    :rtype: Tuple of 3 DataFrames
-    """
-
-    # load the data
-    train_data = pd.read_csv(TRAINING_DATA_PATH)
-    test_data = pd.read_csv(TEST_DATA_PATH)
-    
-    # TODO: figure out the right split so that the test set isn't empty
-    # remove cases that overlap
-    # use a list to modify the test frame after iteration
-    # overlapping_cases = []
-    # for case_id in test_data[CASE_ID_COLUMN].unique():
-    #     if case_id in train_data[CASE_ID_COLUMN] :
-    #         # print(case_id)
-    #         overlapping_cases.append(case_id)
-            
-    # for case_id in overlapping_cases:
-    #     train_data = train_data[train_data[CASE_ID_COLUMN] != case_id]
-    #     test_data = test_data[test_data[CASE_ID_COLUMN] != case_id]
-
-    # Ideally this should not be zero
-    # print(test_data.size)
-
-    # change the event time:timestamp to datetime
-    train_data[CASE_TIMESTAMP_COLUMN] = pd.to_datetime(
-        train_data[CASE_TIMESTAMP_COLUMN])
-    test_data[CASE_TIMESTAMP_COLUMN] = pd.to_datetime(
-        test_data[CASE_TIMESTAMP_COLUMN])
-
-    # get the event that follows the current one for each case (row), and the time elapsed
-    train_data['time until next event'] = - train_data.groupby(
-        CASE_ID_COLUMN)[CASE_TIMESTAMP_COLUMN].diff(-1).dt.total_seconds()
-    train_data['next event'] = train_data.groupby(
-        CASE_ID_COLUMN)[CASE_POSITION_COLUMN].shift(-1)
-    test_data['time until next event'] = - test_data.groupby(
-        CASE_ID_COLUMN)[CASE_TIMESTAMP_COLUMN].diff(-1).dt.total_seconds()
-    test_data['next event'] = test_data.groupby(
-        CASE_ID_COLUMN)[CASE_POSITION_COLUMN].shift(-1)
-    # TODO: add validation data set
-    return train_data, None, test_data
-
+import splitter, constants
 
 def train_baseline_model(train_data_in):
     """Trains the baseline prediction model. It uses the current case position
@@ -72,20 +24,20 @@ def train_baseline_model(train_data_in):
     train_data = copy.deepcopy(train_data_in)
 
     # get the most frequent event that follows a certain event
-    next_event_df = pd.DataFrame(train_data.groupby(CASE_POSITION_COLUMN)[
+    next_event_df = pd.DataFrame(train_data.groupby(constants.CASE_POSITION_COLUMN)[
         'next event'].agg(pd.Series.mode))
     next_event_df.rename(
         columns={'next event': 'predicted next event'}, inplace=True)
     train_data = train_data.merge(
-        next_event_df, how='left', on=CASE_POSITION_COLUMN)
+        next_event_df, how='left', on=constants.CASE_POSITION_COLUMN)
 
     # get the average of the time elapsed after a certain event
-    time_elapsed_df = pd.DataFrame(train_data.groupby(CASE_POSITION_COLUMN)[
+    time_elapsed_df = pd.DataFrame(train_data.groupby(constants.CASE_POSITION_COLUMN)[
         'time until next event'].agg('mean'))
     time_elapsed_df.rename(
         columns={'time until next event': 'predicted time until next event'}, inplace=True)
     train_data = train_data.merge(
-        time_elapsed_df, how='left', on=CASE_POSITION_COLUMN)
+        time_elapsed_df, how='left', on=constants.CASE_POSITION_COLUMN)
 
     # this datafarame stores all the rows where time until next event and next event are null
     # we should decide what to do with it, for now i remove missing values
@@ -103,16 +55,20 @@ def train_baseline_model(train_data_in):
 
 
 def main():
-    train_data, validation_data, test_data = load_dataset()
+    # do this if the files are not split already
+    # splitter.split_dataset(0.2)
+    
+    train_data = pd.read_csv(constants.TRAINING_DATA_PATH)
+    test_data = pd.read_csv(constants.TEST_DATA_PATH)
 
     baseline_next_event_df, baseline_time_elapsed_df = train_baseline_model(
         train_data)
 
     # make predictions for test set
     test_data = test_data.merge(
-        baseline_next_event_df, how='left', on=CASE_POSITION_COLUMN)
+        baseline_next_event_df, how='left', on=constants.CASE_POSITION_COLUMN)
     test_data = test_data.merge(
-        baseline_time_elapsed_df, how='left', on=CASE_POSITION_COLUMN)
+        baseline_time_elapsed_df, how='left', on=constants.CASE_POSITION_COLUMN)
     # also remove missing values:
     test_data.dropna(inplace=True)
 
--- splitter.py
+++ splitter.py
@@ -4,42 +4,58 @@ import copy
 import numpy as np
 import pandas as pd
 
-def split_dataset(split_ratio: float):
-    """Converts the raw dataset into .csv files, so that the number of cases
-    in each is as close as possible to the parametrized value.
+def split_dataset(test_split_ratio: float):
+    """Converts the raw dataset into test and train .csv files, 
+    so that the relation of cases in each is as close as possible 
+    to the parametrized value and overlapping cases are removed.
     The csv files are saved in the same directory as the raw dataset.
 
+    :param test_split_ratio: target portion of cases not dropped
+        in the test dataset
+    :type split_ratio: float
     """
     dataframe = pm4py.convert_to_dataframe(pm4py.read_xes(constants.RAW_DATASET_PATH))
-    pd.set_option('display.max_columns', None)
+
+    # set when printing head to see all columns 
+    # pd.set_option('display.max_columns', None)
 
     # change the event time:timestamp to datetime
     dataframe[constants.CASE_TIMESTAMP_COLUMN] = pd.to_datetime(
         dataframe[constants.CASE_TIMESTAMP_COLUMN])
+
     dataframe.sort_values(by=[constants.CASE_TIMESTAMP_COLUMN], inplace=True)
+
     # get the event that follows the current one for each case (row), and the time elapsed
     dataframe['time until next event'] = - dataframe.groupby(
         constants.CASE_ID_COLUMN)[constants.CASE_TIMESTAMP_COLUMN].diff(-1).dt.total_seconds()
     dataframe['next event'] = dataframe.groupby(
         constants.CASE_ID_COLUMN)[constants.CASE_POSITION_COLUMN].shift(-1)
+
+    # get the number of the current event in the case (starts at 0)
     dataframe[constants.CASE_STEP_NUMBER_COLUMN] = dataframe.groupby(
         constants.CASE_ID_COLUMN).cumcount()
 
     countframe = copy.deepcopy(dataframe)
 
-    # constants are local because we will drop the column by the end
+    # constants are local because we will drop the columns by the end
     CASE_STEP_NUMBER_INVERSE_COLUMN = 'number of activity in case inverse'
     CASE_START_COUNT = 'case start count'
     CASE_END_COUNT = 'case end count'
-    countframe[constants.CASE_STEP_NUMBER_COLUMN] = countframe.groupby(
-        constants.CASE_ID_COLUMN).cumcount()
+
+    # how many events are left until the end of the case
     countframe[CASE_STEP_NUMBER_INVERSE_COLUMN] = countframe.groupby(
         constants.CASE_ID_COLUMN).cumcount(ascending=False)
+
+    # get only the events that start or end a case
     countframe = countframe[(countframe[constants.CASE_STEP_NUMBER_COLUMN] == 0) |
         (countframe[CASE_STEP_NUMBER_INVERSE_COLUMN] == 0)]
+
+    # how many cases start after the current event, inclusive
     countframe[CASE_START_COUNT] = countframe[
         countframe[constants.CASE_STEP_NUMBER_COLUMN] == 0].groupby(
             constants.CASE_STEP_NUMBER_COLUMN).cumcount(ascending=False) + 1
+
+    # how many cases end before the current event, inclusive
     countframe[CASE_END_COUNT] = countframe[
         countframe[CASE_STEP_NUMBER_INVERSE_COLUMN] == 0].groupby(
             CASE_STEP_NUMBER_INVERSE_COLUMN).cumcount() + 1
@@ -47,12 +63,20 @@ def split_dataset(split_ratio: float):
     countframe[CASE_START_COUNT].fillna(method='ffill', inplace = True)
     countframe[CASE_END_COUNT].fillna(method='ffill', inplace = True)
     
-    # filter out only the points that yield a test set of at least the split ratio
-    countframe = countframe[countframe[CASE_START_COUNT]/(countframe[CASE_START_COUNT] + 
-        countframe[CASE_END_COUNT]) > split_ratio]
 
-    # get the timestamp of the last such point, as it yields the smallest test set
-    split_timestamp = countframe.iloc[-1][constants.CASE_TIMESTAMP_COLUMN]
+    # NOTE: this way of filtering possibly throws away 1 case more than neeeded
+    # but rewriting it to not do so would add like 20 lines of code
+
+    # filter out only the points that yield a test set less than the split ratio
+    countframe = countframe[countframe[CASE_START_COUNT] / 
+        (countframe[CASE_START_COUNT] + countframe[CASE_END_COUNT]) < test_split_ratio]
+
+    # get the timestamp of the first such point,
+    # as it yields the smallest test set that doesn't satisfy us
+    # if it's removed from the test set
+    split_timestamp = countframe.iloc[0][constants.CASE_TIMESTAMP_COLUMN]
+
+    # split the data
     train_data = copy.deepcopy(dataframe[dataframe[constants.CASE_TIMESTAMP_COLUMN] < split_timestamp])
     test_data = copy.deepcopy(dataframe[dataframe[constants.CASE_TIMESTAMP_COLUMN] >= split_timestamp])
     
@@ -60,19 +84,19 @@ def split_dataset(split_ratio: float):
     # use a list to modify the test frame after iteration
     overlapping_cases = []
     for case_id in test_data[constants.CASE_ID_COLUMN].unique():
-        if case_id in train_data[constants.CASE_ID_COLUMN] :
-            print(case_id)
+        if case_id in train_data[constants.CASE_ID_COLUMN].values :
             overlapping_cases.append(case_id)
-            
-    for case_id in overlapping_cases:
-        train_data = train_data[train_data[constants.CASE_ID_COLUMN] != case_id]
-        test_data = test_data[test_data[constants.CASE_ID_COLUMN] != case_id]
+
+    print("cases dropped: ", len(overlapping_cases))
+
+    train_data = train_data[~train_data[constants.CASE_ID_COLUMN].isin(overlapping_cases)]
+    test_data = test_data[~test_data[constants.CASE_ID_COLUMN].isin(overlapping_cases)]
 
     # Ideally this should not be zero
-    print(test_data.size)
+    print("size of test set: ", test_data.size)
 
     train_data.to_csv(constants.TRAINING_DATA_PATH)
     test_data.to_csv(constants.TEST_DATA_PATH)
 
 if __name__ == "__main__":
-    split_dataset(0.5)
+    split_dataset(0.2)
