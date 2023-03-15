448d52cdc4c2c82c7b4c609aed786c1bc3e8b442
WouterMulder9 <97562080+WouterMulder9@users.noreply.github.com>
2023-03-15 18:01:14 +0100

    Reformatting & Generalization
    - The output has been reformatted to contain the timedeltas as number of seconds or number of days
    - The names of some of the columns were changed to better reflect their new values: {weekend: is_weekend, time_since_week_start: seconds_since_week_start, time_to_work_hours: seconds_to_work_hours, time_until_next_holiday: days_until_next_holiday}
    - A new way of importing the holiday dates is used such that the function now only needs the (capitalised) country as an input and will get all the necessary dates itself
    - By changing the differences earlier to integers, the comparisons are a lot quicker, resulting in a quicker execution of the code

--- global_vars.py
+++ global_vars.py
@@ -1,10 +1,38 @@
 import pandas as pd
 import datetime
+import workalendar
+import country_converter as cc
+import numpy as np
+from typing import Union
 
-def weekend_and_work_hours(dataframe, date_column:str, start_hours:int, stop_hours:int):
+
+def obtain_holidays(country: str, years: Union[list, np.ndarray]):
     """
-    Adds the extra columns regarding to the weekend and working hours
-    
+    Creates a list of all holidays using a country of choice and the year(s) of choice
+
+    :param country: Country of which the holidays need to be computed
+    :type country: string
+    :param years: List with all the years of interest
+    :type years: Union[list, np.ndarray]
+    :return: A list with datetime.date entries for all holidays in that country in those years
+    :rtype: list
+    """
+
+    holidays = []
+
+    for year in years:
+        holidays += [i[0] for i in eval('{}().holidays({})'.format(country, str(year)))]
+        if country == 'Netherlands':
+            holidays.append(datetime.date(year, 12, 24))
+            holidays.append(datetime.date(year, 12, 31))
+
+    return holidays
+
+
+def time_based_columns(dataframe, date_column: str, start_hours: int, stop_hours: int, country='Netherlands'):
+    """
+    Adds the extra columns regarding to the dates on which events happen. These are things like holidays, weekends & working hours
+
     :param dataframe: original imported pandas dataframe
     :type dataframe: pd.DataFrame
     :param date_column: which column the dates of the event are stored
@@ -13,43 +41,45 @@ def weekend_and_work_hours(dataframe, date_column:str, start_hours:int, stop_hou
     :type start_hours: integer
     :param stop_hours: specification of when the work day ends
     :type stop_hours: integer
-    :return: The new complete dataframe with the following columns 
-            time_since_week_start: Time (string) since Monday 00:00
-            weekend: Boolean value if its a weekend or not
-            work_time: Boolean value if its work time or not
-            time_to_work_hours: Time (string) until the next work day starts (or 0 if its during working hours)
-    :rtype: pd.DataFrame
-    """
-    dataframe[date_column] = dataframe[date_column].apply(lambda x: x.replace(tzinfo=None))
-    dataframe['time_since_week_start'] = dataframe[date_column].apply(lambda x: pd.Timedelta(f'{x.total_seconds()}'))
-    dataframe['weekend'] = dataframe[date_column].apply(lambda x: True if 4<x.weekday()<7 else True
-                                                        if x.weekday() == 4 and x.hour>stop_hours-1 else True 
-                                                        if x.weekday() == 0 and x.hour<start_hours else False)
-    dataframe['work_time'] = dataframe[date_column].apply(lambda x: False if 4<x.weekday()<7 else True if start_hours<=x.hour<stop_hours else False)
-    dataframe['time_to_work_hours'] = dataframe[date_column].apply(lambda x: pd.Timedelta('0h0m') if x.weekday()<5 and start_hours<=x.hour<stop_hours else
-                                                                       datetime.datetime.combine(x.date(), datetime.time(start_hours,0,0,0)) - x if x.weekday()<5 and start_hours>x.hour else
-                                                                       datetime.datetime.combine(x.date()+datetime.timedelta(days = 1), datetime.time(start_hours,0,0,0)) - x if x.weekday()<4 and stop_hours<=x.hour else
-                                                                        datetime.datetime.combine((x.date()+datetime.timedelta(days = 7-x.weekday())), datetime.time(start_hours,0,0,0)) - x) 
-    return dataframe
-
-def national_holidays(dataframe, date_column:str, holiday_dates:list):
-    """
-    Adds the national holidays to a dataframe by using a pre-provided list of holidays
-    
-    :param dataframe: original imported pandas dataframe
-    :type dataframe: pd.DataFrame
-    :param date_column: which column the dates of the event are stored
-    :type date_column: string
-    :param holiday_dates: List with all the holiday dates which happen
-    :type holiday_dates: list
-    :return: New complete dataframe with the following columns added
-            time_until_holiday: Day (string) until the next holiday starts
+    :param country: Country of which the holidays need to be computed
+    :type country: string
+    :return: The new complete dataframe with the following columns
+            seconds_since_week_start: seconds (float) since Monday 00:00
+            is_weekend: Boolean value if its a weekend or not
+            is_work_time: Boolean value if its work time or not
+            seconds_to_work_hours: seconds (float) until the next work day starts (or 0 if its during working hours)
+            days_until_holiday: Days (int) until the next holiday starts
             is_holiday: Boolean value if its a national holiday or not
     :rtype: pd.DataFrame
     """
+    exec('from workalendar.' + cc.convert(country, to='Continent').lower() + ' import ' + country, globals())
+
+    dataframe[date_column] = pd.to_datetime(dataframe[date_column])
+
+    years = np.unique(dataframe[date_column].dropna().dt.year)
+    holidays = obtain_holidays(country=country, years=years)
     dataframe[date_column] = dataframe[date_column].apply(lambda x: x.replace(tzinfo=None))
-    dataframe['time_until_next_holiday'] = dataframe[date_column].apply(lambda x: pd.Timedelta('0h0m') if x.date() in holiday_dates else 
-                                                                            min(holiday - x.date() for holiday in holiday_dates if (holiday - x.date())>pd.Timedelta('0 days')))
-    dataframe['is_holiday'] = dataframe[date_column].apply(lambda x: True if x.date() in holiday_dates else False)
+
+    dataframe['days_until_next_holiday'] = dataframe[date_column].apply(lambda x: 0 if x.date() in holidays else
+    min((holiday - x.date()).days for holiday in holidays if (holiday - x.date()).days > 0))
+    dataframe['is_holiday'] = dataframe[date_column].apply(lambda x: True if x.date() in holidays else False)
+
+    dataframe['seconds_since_week_start'] = dataframe[date_column].apply(
+        lambda x: pd.Timedelta('{} days {}'.format(x.weekday(), x.time())).total_seconds())
+    dataframe['is_work_time'] = dataframe[date_column].apply(
+        lambda x: False if 4 < x.weekday() < 7 else True if start_hours <= x.hour < stop_hours else False)
+    dataframe['seconds_to_work_hours'] = dataframe[date_column].apply(
+        lambda x: pd.Timedelta('0h0m').total_seconds() if x.weekday() < 5 and start_hours <= x.hour < stop_hours else
+        (datetime.datetime.combine(x.date(), datetime.time(start_hours, 0, 0,
+                                                           0)) - x).total_seconds() if x.weekday() < 5 and start_hours > x.hour else
+        (datetime.datetime.combine(x.date() + datetime.timedelta(days=1), datetime.time(start_hours, 0, 0,
+                                                                                        0)) - x).total_seconds() if x.weekday() < 4 and stop_hours <= x.hour else
+        (datetime.datetime.combine((x.date() + datetime.timedelta(days=7 - x.weekday())),
+                                   datetime.time(start_hours, 0, 0, 0)) - x).total_seconds())
+
+    dataframe['is_weekend'] = dataframe[date_column].apply(lambda x: True if 4 < x.weekday() < 7 else True
+    if x.weekday() == 4 and x.hour > stop_hours - 1 else True
+    if x.weekday() == 0 and x.hour < start_hours else False)
     return dataframe
+
 
