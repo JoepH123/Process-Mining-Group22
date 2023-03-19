5cce6c73c2788e6453a2766c1fdb8822f1086436
WouterMulder9 <97562080+WouterMulder9@users.noreply.github.com>
2023-03-19 22:26:01 +0100

    Included the Variable for the active cases per resource in the new dataset with all the other variables obtained from global_vars.py

--- global_vars.py
+++ global_vars.py
@@ -4,6 +4,8 @@ import workalendar
 import country_converter as cc
 import numpy as np
 from typing import Union
+import warnings
+from tqdm import tqdm
 
 
 def obtain_holidays(country: str, years: Union[list, np.ndarray]):
@@ -92,3 +94,168 @@ def add_liquidity(df, date_column):
     df['total_amount'] = df.groupby(['year', 'month', 'day'], as_index=False).agg(total_amount=('case:AMOUNT_REQ', 'sum'))['total_amount']
     
     return df
+
+
+def resource_active_cases(dataframe, time_col='time:timestamp', event_col='concept:name',
+                          status_col='lifecycle:transition', resource_col='org:resource',
+                          time_between_events_col='time until next event', case_id_col='case:concept:name',
+                          act_nr_col='activity number in case', next_event_col='next event',
+                          activity_method='w_activities', start_variable='START'):
+    '''
+    Function to obtain all the active cases for a resource at the time of starting a new event.
+    Two different approaches can be used to calculate this number; based on only the work activities,
+    as they have a start and end point and based on the average duration for all entries
+
+    :param dataframe: Original imported dataframe
+    :type dataframe: pd.DataFrame
+    :param time_col: Column name of the provided dataframe which contains the time when an event started
+    :type time_col: string
+    :param event_col: Column name of the provided dataframe which contains the type of event
+    :type event_col: string
+    :param status_col: Column name of the provided dataframe which contains the status of an event at the point of database entry
+    :type status_col: string
+    :param resource_col: Column name of the provided dataframe which contains the recourse which works on the event
+    :type resource_col: string
+    :param time_between_events_col: Column name of the provided dataframe which contains the time between the current and the next event
+    :type time_between_events_col: string
+    :param case_id_col: Column name of the provided dataframe which contains the identifier of the case
+    :type case_id_col: string
+    :param act_nr_col: Column name of the provided dataframe which contains the number of preceding events when looking at the total sequence of activities for the current case
+    :type act_nr_col: string
+    :param next_event_col: Column name of the provided dataframe which contains the type of event for the next event in the sequence for the case
+    :type next_event_col: string
+    :param activity_method: Entry which is used to determine which method is chosen to determine all the possible active cases. The following methods can be chosen: \n
+        w_activities: In this case only the activities which have a start and end point within the input dataset are used to obtain the active cases. \n
+        all_activities: In this case all the activities in the dataset are used to determine the active cases, however as not all entries might have a determined start and end point, the median time is used to determine the duration of the events. \n
+        both: In this case, both of the aforementioned methods are used to return both outcomes. \n
+    :type time_col: string
+    :param start_variable: How the start of a case is indicated in the status column of the given dataset
+    :return: The new dataframe with the number of active activites for each resource at the start of the event for all event entries
+    :rtype: pd.Dataframe
+    '''
+    warnings.warn('Function is very costly, so it might take a while to produce the results')
+    if activity_method == 'w_activities':
+        for index in tqdm(dataframe[dataframe[status_col] == start_variable].index):
+            broken = False
+            or_index = index
+            case_id = dataframe.at[index, case_id_col]
+            act_nr = dataframe.at[index, act_nr_col]
+            target = act_nr + 1
+
+            while dataframe.at[or_index, event_col] != dataframe.at[index, next_event_col]:
+                try:
+                    index = dataframe[(dataframe[case_id_col] == case_id) & (dataframe[act_nr_col] == target)].index[0]
+                    target += 1
+
+                except:
+                    dataframe.at[or_index, 'w_end_time'] = pd.NaT
+                    broken = True
+                    break
+
+            if broken:
+                continue
+            else:
+                dataframe.at[or_index, 'w_end_time'] = dataframe.at[
+                    dataframe[(dataframe[case_id_col] == case_id) & (dataframe[act_nr_col] == target)].index[0], time_col]
+
+        dataframe[resource_col] = dataframe[resource_col].fillna(0)
+        work_frame = dataframe[['w_end_time', time_col, resource_col]].copy()
+        for index in tqdm(work_frame.index):
+            resource = work_frame.at[index, resource_col]
+            time_event = work_frame.at[index, time_col]
+            dataframe.at[index, 'active_resource_cases'] = len(set.intersection(set(
+                np.where((work_frame['w_end_time'] > time_event) & (work_frame[resource_col] == resource))[0].tolist()),
+                                                                                set(np.where((work_frame[
+                                                                                                  time_col] < time_event) & (
+                                                                                                         work_frame[
+                                                                                                             resource_col] == resource))[
+                                                                                        0].tolist())))
+
+        return dataframe.drop('w_end_time', axis=1)
+
+    elif activity_method == 'all_activities':
+        median_duration = dataframe.groupby(event_col).median()[time_between_events_col].to_dict()
+        for index in dataframe.index:
+            dataframe.at[index, 'end_time'] = dataframe.at[index, time_col] + datetime.timedelta(
+                seconds=median_duration[dataframe.at[index, event_col]])
+
+        dataframe[resource_col] = dataframe[resource_col].fillna(0)
+        work_frame = dataframe[['end_time', time_col, resource_col]].copy()
+        for index in tqdm(work_frame.index):
+            resource = work_frame.at[index, resource_col]
+            time_event = work_frame.at[index, time_col]
+            dataframe.at[index, 'active_resource_cases'] = len(set.intersection(set(
+                np.where((work_frame['end_time'] > time_event) & (work_frame[resource_col] == resource))[0].tolist()),
+                                                                                set(np.where((work_frame[
+                                                                                                  time_col] < time_event) & (
+                                                                                                         work_frame[
+                                                                                                             resource_col] == resource))[
+                                                                                        0].tolist())))
+
+        return dataframe.drop('end_time', axis=1)
+
+    elif activity_method == 'both':
+        median_duration = dataframe.groupby(event_col).median()[time_between_events_col].to_dict()
+        for index in dataframe.index:
+            dataframe.at[index, 'end_time'] = dataframe.at[index, time_col] + datetime.timedelta(
+                seconds=median_duration[dataframe.at[index, event_col]])
+
+        for index in tqdm(dataframe[dataframe[status_col] == start_variable].index):
+            broken = False
+            or_index = index
+            case_id = dataframe.at[index, case_id_col]
+            act_nr = dataframe.at[index, act_nr_col]
+            target = act_nr + 1
+
+            while dataframe.at[or_index, event_col] != dataframe.at[index, next_event_col]:
+                try:
+                    index = dataframe[(dataframe[case_id_col] == case_id) & (dataframe[act_nr_col] == target)].index[0]
+                    target += 1
+
+                except:
+                    dataframe.at[or_index, 'w_end_time'] = pd.NaT
+                    broken = True
+                    break
+
+            if broken:
+                continue
+            else:
+                dataframe.at[or_index, 'w_end_time'] = dataframe.at[
+                    dataframe[(dataframe[case_id_col] == case_id) & (dataframe[act_nr_col] == target)].index[0], time_col]
+
+        dataframe[resource_col] = dataframe[resource_col].fillna(0)
+        work_frame = dataframe[['end_time', 'w_end_time', time_col, resource_col]].copy()
+        for index in tqdm(work_frame.index):
+            resource = work_frame.at[index, resource_col]
+            time_event = work_frame.at[index, time_col]
+            dataframe.at[index, 'active_resource_cases(all)'] = len(set.intersection(set(
+                np.where((work_frame['end_time'] > time_event) & (work_frame[resource_col] == resource))[0].tolist()),
+                                                                                     set(np.where((work_frame[
+                                                                                                       time_col] < time_event) & (
+                                                                                                              work_frame[
+                                                                                                                  resource_col] == resource))[
+                                                                                             0].tolist())))
+            dataframe.at[index, 'active_resource_cases(w)'] = len(set.intersection(set(
+                np.where((work_frame['w_end_time'] > time_event) & (work_frame[resource_col] == resource))[0].tolist()),
+                                                                                   set(np.where((work_frame[
+                                                                                                     time_col] < time_event) & (
+                                                                                                            work_frame[
+                                                                                                                resource_col] == resource))[
+                                                                                           0].tolist())))
+
+        return dataframe.drop(['end_time', 'w_end_time'], axis=1)
+
+    else:
+        raise ValueError('Unknown method used, try one of the following methods: [w_activities, all_activities, both]')
+
+if __name__ == '__main__':
+    old_frame = pd.read_csv('Datasets/BPI_Challenge_2012.csv', index_col = 'Unnamed: 0')
+    old_frame.drop(old_frame.index[-1], inplace=True)
+    old_frame.rename_axis(None, axis=0, inplace=True)
+    old_frame['case:REG_DATE'] = pd.to_datetime(old_frame['case:REG_DATE'])
+    old_frame['time:timestamp'] = pd.to_datetime(old_frame['time:timestamp'])
+    vacs = time_based_columns(old_frame, 'time:timestamp', 9, 17)
+    liq = add_liquidity(vacs, 'time:timestamp')
+    final = resource_active_cases(liq, activity_method='both')
+    print(final)
+    final.to_csv('Datasets/BPI_Challenge_2012-global_vars.csv')
